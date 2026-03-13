d5a72b407052d24af1f1bada9c3d4cf01af9209b
Amulya_S_A <118207243+AMULYA717@users.noreply.github.com>
2026-03-13 11:15:22 +0530

    Update connect.js

--- Devops-master/src/db/connect.js
+++ Devops-master/src/db/connect.js
@@ -1,10 +1,10 @@
-const mongoose=require("mongoose");
-mongoose.connect("mongodb+srv://anaghakumbaleks:Anagha1605@cluster0.c14nklq.mongodb.net/",{
-    useNewUrlParser:true,
-    useUnifiedTopology:true,
-    
-}).then(()=>{
-    console.log(` register conn is successful`);
-}).catch((err)=>{
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log("DB connection successful");
+}).catch((err) => {
     console.log(err);
-})
+});
